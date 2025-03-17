9f9860447c003311ae8dceabaeb5556e26c84dfc
Halo <101888522+fullC19H28O2@users.noreply.github.com>
2025-03-17 21:45:01 +0300

    Initial commit: Added backend and frontend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log("MongoDB Connection Error:", err));
+
+
+app.get("/", (req,res)=>{
+    res.send("Finance APP API Running...");
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
+
+const authRoutes = require("./routes/auth");
+app.use("/api/auth", authRoutes);
+
+const userRoutes = require("./routes/user");
+app.use("/api/user", userRoutes);
